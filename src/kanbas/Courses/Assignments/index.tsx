8df336ece143542f7e1095a5e3e6f3319b97e12d
akshita-as02 <akshita.as02@gmail.com>
2024-10-03 22:21:07 -0400

    a2 CSS and Bootstrap

--- src/kanbas/Courses/Assignments/index.tsx
+++ src/kanbas/Courses/Assignments/index.tsx
@@ -10,49 +10,53 @@ export default function Assignments() {
                     <span className="input-group-text">
                         <FaSearch />
                     </span>
-                    <input 
+                    <input
                         id="wd-search-assignment"
                         className="form-control"
                         placeholder="Search for Assignments"
                     />
                 </div>
                 <div>
-                    <button 
-                        id="wd-add-assignment-group" 
-                        className="btn btn-light me-2" 
+                    <button
+                        id="wd-add-assignment-group"
+                        className="btn btn-light me-2"
                         style={{ color: 'black' }}
                     >
                         + Group
                     </button>
-                    <button 
-                        id="wd-add-assignment" 
+                    <button
+                        id="wd-add-assignment"
                         className="btn btn-danger"
                     >
                         + Assignment
                     </button>
                 </div>
             </div>
-            
-            {/* Assignments Title */}
-            <h3 id="wd-assignments-title" className="d-flex justify-content-between align-items-center">
-                ASSIGNMENTS 40% of Total
-                <button className="btn btn-outline-secondary">
-                    <FaPlus />
-                </button>
+            <h3
+                id="wd-assignments-title"
+                className="d-flex justify-content-between align-items-center bg-secondary text-white p-3 rounded"
+                style={{ backgroundColor: 'grey', borderRadius: '8px' }}
+            >
+                <span className="fw-bold" style={{ fontSize: '1.5rem', color: "black" }}>ASSIGNMENTS</span>
+                <div className="d-flex align-items-center">
+                    <span className="small me-3" style={{ fontSize: '0.9rem', color: 'black' }}>40% of Total</span>
+                    <button className="btn btn-outline-secondary" style={{ color: 'black' }}>
+                        <FaPlus />
+                    </button>
+                </div>
             </h3>
-            
-            {/* Assignments List */}
+
             <ul id="wd-assignment-list" className="list-unstyled">
                 {/* Assignment 1 */}
                 <li className="wd-assignment-list-item d-flex align-items-start">
                     <div className="wd-icon">
-                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'blue' }} />
+                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'green' }} />
                     </div>
                     <div className="flex-grow-1">
-                        <a 
+                        <a
                             className="wd-assignment-link"
                             href="#/Kanbas/Courses/1234/Assignments/123"
-                            style={{ fontWeight: 'bold', color: 'blue' }}
+                            style={{ fontWeight: 'bold', color: 'black' }}
                         >
                             A1 - ENV + HTML
                         </a>
@@ -69,13 +73,13 @@ export default function Assignments() {
                 {/* Assignment 2 */}
                 <li className="wd-assignment-list-item d-flex align-items-start">
                     <div className="wd-icon">
-                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'blue' }} />
+                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'green' }} />
                     </div>
                     <div className="flex-grow-1">
-                        <a 
+                        <a
                             className="wd-assignment-link"
                             href="#/Kanbas/Courses/1234/Assignments/124"
-                            style={{ fontWeight: 'bold', color: 'blue' }}
+                            style={{ fontWeight: 'bold', color: 'black' }}
                         >
                             A2 - CSS + BOOTSTRAP
                         </a>
@@ -92,13 +96,13 @@ export default function Assignments() {
                 {/* Assignment 3 */}
                 <li className="wd-assignment-list-item d-flex align-items-start">
                     <div className="wd-icon">
-                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'blue' }} />
+                        <MdOutlineAssignment style={{ fontSize: '1.5rem', color: 'green' }} />
                     </div>
                     <div className="flex-grow-1">
-                        <a 
+                        <a
                             className="wd-assignment-link"
                             href="#/Kanbas/Courses/1234/Assignments/125"
-                            style={{ fontWeight: 'bold', color: 'blue' }}
+                            style={{ fontWeight: 'bold', color: 'black' }}
                         >
                             A3 - JAVASCRIPT + REACT
                         </a>
